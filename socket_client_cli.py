4d2f0abd200ba78a0c3df69f698abb58f3b6079e
Tommaso Paoli <paoli7612@gmail.com>
2018-05-06 17:01:55 +0200

    clear client cli

--- socket_client_cli.py
+++ socket_client_cli.py
@@ -1,20 +1,23 @@
-# -*- coding:utf-8 -*-
-
 import cmd
+from parking_simulation import SocketClient
 
-class BaseCli(cmd.Cmd):
-
+class Cli(cmd.Cmd):
     def __init__(self):
         cmd.Cmd.__init__(self)
         self.prompt = '> '
+        with open("port.txt") as file:
+            self.port = int(file.read())
 
     def do_quit(self, arg):
-        print "\nbye bye"
+        print("\nbye bye")
         return 1
 
-    def do_EOF(self, arg):
-        self.do_quit(None)
-        return 1
+    def do_state(self, line):
+        client = SocketClient(port=self.port)
+        data = client.get_data()
+        mess = "STATE: %s " % data.decode()
+        print(mess)
+        del client
 
     def emptyline(self):
         pass
@@ -22,29 +25,6 @@ class BaseCli(cmd.Cmd):
     def run(self):
         self.cmdloop()
 
-#
-import cmd
-from parking_simulation import SocketClient
-class ParkSocketCli(BaseCli):
-
-    def __init__(self):
-        BaseCli.__init__(self)
-        with open("port.txt") as file:
-            self.port = int(file.read())
-
-    def do_state(self, line):
-        client = SocketClient(port=self.port)
-        data = client.get_data()
-        mess = "STATE: %s " % data
-        print(mess)
-        del client
-
-def main(args):
-    cli = ParkSocketCli()
-    cli.run()
-
 if __name__ == "__main__":
-
-    import sys
-    args = sys.argv[1:]
-    sys.exit(main(args))
+    cli = Cli()
+    cli.run()
